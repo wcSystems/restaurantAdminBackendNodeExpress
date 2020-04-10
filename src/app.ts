27adc611c3ce591670bf7e9ae626a054f1fe77e0
Willinthon Carriedo <anthonycarriedo2013@gmail.com>
2020-04-10 05:09:39 -0300

    ddd

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app: Application = express();
 
 // settings
 app.set('port', process.env.PORT || 4000);
-app.set('url', process.env.DIST_FOLDER);
+// app.set('url', process.env.DIST_FOLDER);
 
 // middlewares
 app.use(morgan('dev'));
